956f6de75fa1500142e018bcd7f9cf2a15b0d8fe
wentingchua <wentingchua@gmail.com>
2025-10-17 21:25:36 -0400

    Add php date and time tutorials

---
+++ CSC209/Homework/Hw6_7/Tutorials/dateTime.html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+    ?>
+
+    © 2010-<?php echo date("Y"); ?>
+
+    <?php
+    $d = strtotime("tomorrow");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+    $d = strtotime("next Saturday");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+    $d = strtotime("+3 Months");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+    ?>
+
+</body>
+
+</html>
